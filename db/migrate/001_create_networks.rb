36c11fd8674e188938656c31c2f01ee05ca0d3d4
Mika88 <mikaspivak@gmail.com>
2018-10-16 23:57:30 +0000

    automatically backed up by learn

--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -2,8 +2,6 @@ class CreateNetworks < ActiveRecord::Migration[4.2]
   def change
     create_table :networks do |t|
       t.string :call_letters
-      # the above breaks down to
-      # "create a column called :call_letters on table t with type string
       t.integer :channel
     end
   end
